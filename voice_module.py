09aefa2c38cbab6cb75b1bf4489ddab4ac807d1c
Amreta-mp <148216966+Amreta-mp@users.noreply.github.com>
2025-04-05 23:54:11 +0530

    Create voice_module.py

---
+++ voice_module.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+def transcribe_voice(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        return "Could not understand. Please try again."
